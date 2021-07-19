ba4ccb19297bd616e3c7cbf202f2121c62de24d8
Carl Farterson <carlfarterson@gmail.com>
2021-07-19 16:55:07 -0700

    Fix: hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 module.exports = {
+    defaultNetwork: "mainnet",
     solidity: {
         compilers: [
             { version: "0.8.0" }
@@ -16,25 +17,18 @@ module.exports = {
         timeout: 60000 // Here is 2min but can be whatever timeout is suitable for you.
     },
     networks: {
-        hardhat: {
-            forking: {
-                url: `https://eth-mainnet.alchemyapi.io/v2/...`,
-                accounts: [`...`,]
-            },
-            gas: 'auto',
-            blockGasLimit: 0x1fffffffffffff,
-            allowUnlimitedContractSize: true,
-            timeout: 1800000,
-        },
         mainnet: {
-            url: `https://eth-mainnet.alchemyapi.io/v2/...`,
-            accounts: [`...`,]
+            url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+            accounts: [`${process.env.PRIVATE_KEY}`,]
         },
-
+        rinkeby: {
+            url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+            accounts: [`${process.env.PRIVATE_KEY}`,]
+        },
     },
     etherscan: {
-        url: String(`https://eth-mainnet.alchemyapi.io/v2/...`),
-        apiKey: `...`
+        url: String(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`),
+        apiKey: process.env.ETHERSCAN_KEY
     },
     paths: {
         sources: "./contracts",
